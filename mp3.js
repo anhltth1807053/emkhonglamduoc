3113907cd55d433672c506d30540c6ef1abba842
anhltth1807053 <anhltth1807053@fpt.edu.vn>
2018-10-30 15:26:50 +0700

    Update mp3.js

---
+++ mp3.js
@@ -0,0 +1,16 @@
+
+var baihat1= {
+    "id":1540304588828,
+    "name":"TÌnh Ca ",
+    "description":"Tình Ca",
+    "singer":"Trọng Hiếu",
+    "author":"Quốc Bảo",
+    "thumbnail":"http://static.amusic.vn/amusic/albums/s2/10/82660/82660.jpg?v=1523961932",
+    "link":"https://s1.vocaroo.com/media/download_temp/Vocaroo_s1cqKcLqsxJ5.mp3",
+    "memberId":1538655637969,
+    "createdAt":"2018-10-23T14:23:08.828+0000",
+    "updatedAt":"2018-10-23T14:23:08.828+0000",
+    "status":1
+},
+
+
